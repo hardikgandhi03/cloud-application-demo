38385d5201a5a0a4bd3a2f5c74ca71198ae079bc
hardikgandhi03 <gandhi.hardik2001@gmail.com>
2022-06-17 16:12:33 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+db.collection('Users').add({'name':'Aman', 'city':'Vadodara'})
+
+
